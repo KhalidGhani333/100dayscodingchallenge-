052b1dccf308c5261c1c43e3e3a582f28a7968f1
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-22 14:24:21 +0500

    Add files via upload

---
+++ day-17/q50/main.js
@@ -0,0 +1,4 @@
+//Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day.
+// Include at least three different activities.
+var myInfo = "My Personal Information:\nMy name is Khalid Ghani.\nMy father name is Akhtar Shah.\nI live in Karachi Pakistan.\nI'm the student of Governor Sindh Initiative Program.\n";
+console.log(myInfo);
---
+++ day-17/q50/main.ts
@@ -0,0 +1,9 @@
+//Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day.
+// Include at least three different activities.
+let myInfo = `My Personal Information:
+My name is Khalid Ghani.
+My father name is Akhtar Shah.
+I live in Karachi Pakistan.
+I'm the student of Governor Sindh Initiative Program.
+`
+console.log(myInfo);

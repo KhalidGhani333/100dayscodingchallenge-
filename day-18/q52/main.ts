6b2be62ed9b223d471fcd2f3a2dd767899c81bde
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-22 14:27:12 +0500

    Add files via upload

---
+++ day-18/q52/main.ts
@@ -0,0 +1,10 @@
+//Make a Smartphone Object: Create a simple way to keep track of a smartphone's details.
+// Include its brand, model, and other key features like how much storage it has,
+//the size of its screen, and how long its battery lasts.
+
+let smartPhone ={
+    brand:"Oppo",
+    model:"A5s",
+    ketFeatures:{Storage:"125GB",size:"6.2 inches",battery:"18 Hours"}
+}
+console.log(smartPhone)

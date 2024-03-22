86ba5680e7fa48372b889897f2ab8704a8f97424
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-22 14:21:35 +0500

    Add files via upload

---
+++ day-16/q48/main.ts
@@ -0,0 +1,12 @@
+//Combining Arrays with Spread Operator: Suppose you're comparing prices of two different sets of laptops.
+// Use the spread operator to combine these arrays into a single array sorted in ascending order,
+// then log the result.
+
+
+let price1=[1000,3000,5000];
+let price2=[2000,4000,6000];
+let combineprice =[...price1,...price2]
+
+combineprice.sort((a, b) => a - b);
+
+console.log(combineprice);

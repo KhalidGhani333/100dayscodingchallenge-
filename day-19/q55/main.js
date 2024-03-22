da873ec911ad1c0525e59641cc52d86710c6549e
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-22 14:32:25 +0500

    Add files via upload

---
+++ day-19/q55/main.js
@@ -0,0 +1,5 @@
+//Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list
+// where each number is twice its original value.
+var numberArr = [3, 6, 9, 12, 15];
+var DoubleNumber = numberArr.map(function (numberArr) { return numberArr * 2; });
+console.log(DoubleNumber);
---
+++ day-19/q55/main.ts
@@ -0,0 +1,8 @@
+//Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list
+// where each number is twice its original value.
+
+let numberArr=[3,6,9,12,15]
+
+let DoubleNumber=numberArr.map (numberArr => numberArr*2);
+
+console.log(DoubleNumber)

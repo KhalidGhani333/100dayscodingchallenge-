895082c43ff5710683b33c358c51a4aef968d640
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-24 00:21:01 +0500

    Add files via upload

---
+++ day-22/q65/main.ts
@@ -0,0 +1,8 @@
+//determine the Remainder: Make a function that gets two numbers and shows the leftover
+// from dividing them using the % sign. For example, remainder(5, 2) should give 1.
+
+function remainder(num1:number,num2:number){
+       console.log(num1 % num2)
+}
+
+remainder(10,4)

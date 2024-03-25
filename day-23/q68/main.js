4ed3e011b57ced6e76fd062536ba3a6468b7140f
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-25 13:17:20 +0500

    Add files via upload

---
+++ day-23/q68/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Multiplying Decimals: Create a function that accepts two decimal numbers and returns their product.
+// Round the result to two decimal places.
+function decimalNumber(num1, num2) {
+    return Math.round(num1 * num2);
+}
+console.log(decimalNumber(0.3, 0.4 * 100) / 100);
---
+++ day-23/q68/main.ts
@@ -0,0 +1,9 @@
+//Multiplying Decimals: Create a function that accepts two decimal numbers and returns their product.
+// Round the result to two decimal places.
+
+function decimalNumber(num1:number,num2:number):number{
+    return Math.round (num1 * num2 )
+}
+
+
+console.log(decimalNumber(0.3,0.4 *100)/100)

abffd36d20006af9d8c351433f7c74e3fe418acc
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-25 15:03:22 +0500

    Add files via upload

---
+++ day-24/q71/main.ts
@@ -0,0 +1,17 @@
+//Compare let and const: Create two examples where let allows reassignment but const does not.
+// Try to reassign a const-declared variable and catch the error.
+
+let myage=20;
+myage=21;
+
+console.log(myage)
+
+//it's work fine and change its value from 20 to 21 because let is reassignable variable it not given error.
+
+const age=20;
+ age =21
+
+ console.log(age)
+
+ //it's give us error because const is not reassignable variable
+ // const prevents changing the variable once it's set.

904557bfbc110ec14dcd65e035c984fd47cc5e93
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-26 12:52:15 +0500

    Add files via upload

---
+++ day-25/q74/main.ts
@@ -0,0 +1,15 @@
+//Swapping Variables: Demonstrate how to swap the values of two variables. Start with variables a = 5 and b = 10,
+// then swap their values so that a becomes 10 and b becomes 5.
+
+function swapvalue(){
+    let a=5 , b=10
+    console.log(`before swap: a=${a} and b=${b}`)
+
+    a=10 ,b=5
+    console.log(`after swap: a=${a} and b=${b}`)
+}
+
+
+
+
+swapvalue();

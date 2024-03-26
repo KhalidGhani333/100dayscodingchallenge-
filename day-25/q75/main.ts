29340f58334d2728d298f79bf05ba22a7c231f2c
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-26 12:53:23 +0500

    Add files via upload

---
+++ day-25/q75/main.ts
@@ -0,0 +1,22 @@
+//Compound Assignment Operators: Use compound assignment operators to simplify arithmetic operations.
+// Start with x = 4 and perform a series of operations (addition, subtraction, multiplication, division)
+// on x using compound operators.
+
+function CompoundOperator(){
+    let x=4 
+    console.log(`initial x:  ${x}`)
+
+    x += 6;
+    console.log(`after addition x:  ${x}`)
+    
+    x -= 5;
+    console.log(`after subtraction x:  ${x}`)
+
+    x *= 4;
+    console.log(`after multiplication x:  ${x}`)
+
+    x /= 2;
+    console.log(`after division x:  ${x}`)
+}
+
+CompoundOperator();

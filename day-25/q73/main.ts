a90be9083068085e07fe79249c8211bf2c362267
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-03-26 12:51:22 +0500

    Add files via upload

---
+++ day-25/q73/main.ts
@@ -0,0 +1,12 @@
+//Assigning and Updating Variables: Create a function where you declare a variable using let and assign an initial value.
+// Then, update its value within the same function and log both the initial and updated values.
+
+function updatingVariable(){
+    let Value = 56;
+    console.log(`Initial value: ${Value}`)
+
+   Value = 60;
+    console.log(`updated value: ${Value}`)
+}
+
+updatingVariable();

1a3eeb48f5996941a5249f9a60f5cfa348b3986d
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-03 00:42:56 +0500

    Add files via upload

---
+++ day-29/q87/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+function extractCharacters(str) {
+    return str.substring(0, 15);
+}
+console.log(extractCharacters("We are learning typescript"));
---
+++ day-29/q87/main.ts
@@ -0,0 +1,11 @@
+//Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+
+function extractCharacters(str:string):string{
+    return str.substring(0,15);
+}
+
+
+console.log(extractCharacters("We are learning typescript"))
+
+
+

ed53982cdabcb5f1a999c9e9a2d85855c51da6f9
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-02 12:59:20 +0500

    Add files via upload

---
+++ day-26/q76/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together,
+// and returns the result. Show how you can call this function and log the result.
+function Returnvalue(num1, num2) {
+    return num1 + num2;
+}
+console.log(Returnvalue(20, 30));
---
+++ day-26/q76/main.ts
@@ -0,0 +1,9 @@
+//Function Parameters and Return Values: Create a function that takes two numbers as parameters, adds them together,
+// and returns the result. Show how you can call this function and log the result.
+
+
+function Returnvalue(num1:number,num2:number):number{
+    return num1 + num2 
+}
+
+console.log(Returnvalue(20,30))

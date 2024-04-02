ebf724dd15272e521698fb6dc8d2ea4919fe7883
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-02 13:05:10 +0500

    Add files via upload

---
+++ day-27/q80/main.js
@@ -0,0 +1,12 @@
+"use strict";
+//Updating Object Properties: Add a property named color to the existing car object,
+// and then update the year property to 2021. Show how to perform these operations.
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2022,
+    color: "string"
+};
+car.color = "white"; //we don't add new property to object  
+car.year = 2023;
+console.log(car);

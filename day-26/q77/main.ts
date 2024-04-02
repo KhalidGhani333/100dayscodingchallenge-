cd92f9e9165661a8e52655c36f8a0ab90940ec72
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-02 13:00:02 +0500

    Add files via upload

---
+++ day-26/q77/main.ts
@@ -0,0 +1,10 @@
+//Default Parameters: Write a function that greets a user. It should take the user's name as a parameter and say hello.
+// If no name is given, it should greet an anonymous user.
+
+
+function greetuser(name:string="anonymous"){
+    console.log(`Hello ${name}`)
+}
+
+greetuser("shahzaib")
+greetuser()

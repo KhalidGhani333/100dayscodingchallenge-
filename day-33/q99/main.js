d9339cd5dfd5f63e31c78b0ec34261bed2fd3ee2
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-04 10:27:00 +0500

    Add files via upload

---
+++ day-33/q99/main.js
@@ -0,0 +1,13 @@
+"use strict";
+//Generate a date object representing your next birthday and log it to the console.
+function NextBirthday(month, day) {
+    const today = new Date();
+    let year = today.getFullYear();
+    const birthday = new Date(year, month - 1, day);
+    if (birthday < today) {
+        birthday.setFullYear(year + 1);
+    }
+    return birthday;
+}
+const nextBirthday = NextBirthday(1, 1);
+console.log("Next birthday on:", nextBirthday.toLocaleDateString());

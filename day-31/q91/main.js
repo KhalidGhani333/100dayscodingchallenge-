8b7e03619548ae274b6e7f323428f680f60e8c44
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-04 10:19:22 +0500

    Add files via upload

---
+++ day-31/q91/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// create an array of your three favourite fruits and add a new fruit to the end of the array.
+let fruitArray = ["Mango", "strawberry", "Apple"];
+fruitArray.push("Graphs");
+console.log(fruitArray);
---
+++ day-31/q91/main.ts
@@ -0,0 +1,7 @@
+// create an array of your three favourite fruits and add a new fruit to the end of the array.
+
+let fruitArray=["Mango","strawberry","Apple"]
+
+fruitArray.push("Graphs")
+
+console.log(fruitArray)

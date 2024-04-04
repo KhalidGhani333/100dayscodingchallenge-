a5cd94623ee412f8ac066af4f3b549577ac88f68
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-04 11:26:52 +0500

    Add files via upload

---
+++ day-34/q100/main.js
@@ -0,0 +1,4 @@
+"use strict";
+//Use the JavaScript Math object to find the square root of 144.
+let squareRoot = Math.sqrt(144);
+console.log(squareRoot);
---
+++ day-34/q100/main.ts
@@ -0,0 +1,5 @@
+//Use the JavaScript Math object to find the square root of 144.
+
+let squareRoot = Math.sqrt(144)
+
+console.log(squareRoot)

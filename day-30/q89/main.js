09b3dd4016deb1d57ba092cee574445e11a6cb55
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-04 10:16:40 +0500

    Add files via upload

---
+++ day-30/q89/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Converting Strings to Numbers: Create a function that takes a string representing a number (like "123")
+// and converts it into an actual number type.
+function ConvertingString(num) {
+    return parseInt(num);
+}
+console.log(ConvertingString("89"));
---
+++ day-30/q89/main.ts
@@ -0,0 +1,8 @@
+//Converting Strings to Numbers: Create a function that takes a string representing a number (like "123")
+// and converts it into an actual number type.
+
+function ConvertingString(num:string):number{
+   return parseInt(num)
+}
+
+console.log(ConvertingString("89"))

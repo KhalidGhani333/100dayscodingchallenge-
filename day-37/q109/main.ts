9bf1468a71f05a4e7ed185d1c4c4d2e59646bcb7
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-08 23:06:11 +0500

    Add files via upload

---
+++ day-37/q109/main.ts
@@ -0,0 +1,8 @@
+
+//Write an if statement that logs "Good Morning" if the current time is before 12 PM
+
+let current_Date = new Date()
+
+if(current_Date.getHours() < 12){
+    console.log("Good Morning")
+}

86f6bc48dd4f5b98b6c7742f7b647678ed004ef0
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-08 23:07:36 +0500

    Add files via upload

---
+++ day-37/q111/main.ts
@@ -0,0 +1,14 @@
+
+//Use an if-else-if chain to categorize a person's age group (child, teenager, adult).
+//anyone under 13 a child, between 13 and 19 a teenager, and 20 or older an adult.
+
+
+let personAge = 12
+
+if(personAge >= 20 ){
+    console.log ("Adult")
+}
+else if(personAge >= 13 && personAge <=19 ){
+    console.log ("Teenager")
+}
+else{console.log("Child")}

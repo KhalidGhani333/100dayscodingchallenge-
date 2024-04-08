86fd8d1c9a97665d838f96a98dbaa44279d8d1e2
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-08 23:12:25 +0500

    Add files via upload

---
+++ day-38/q114/main.js
@@ -0,0 +1,13 @@
+"use strict";
+//Iterate over a Map of student IDs and names, and log each pair to the console.
+//*Explain & TIP:* Iterating over a Map can be done using the .forEach() method or a for...of loop,
+// providing access to each key-value pair.
+let students = new Map();
+students.set(1011, "Shahzaib");
+students.set(1012, "Faraz");
+students.set(1013, "Faizan");
+students.set(1014, "Ibrahim");
+students.set(1015, "Saad");
+students.forEach((name1, id) => {
+    console.log(`Student Id:${id} student Name:${name1}`);
+});

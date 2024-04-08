ab4a2dd3422aa464f87a4b95df72a567d7c8f2dc
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-08 23:10:17 +0500

    Add files via upload

---
+++ day-38/q112/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Create a Map that stores the names of countries as keys and their capitals as values. Add three countries to the Map.
+let CountriesCapital = new Map();
+CountriesCapital.set("Pakistan", "Islamabad");
+CountriesCapital.set("Germany", "Berlin");
+CountriesCapital.set("Italy", "Rome");
+console.log(CountriesCapital);
---
+++ day-38/q112/main.ts
@@ -0,0 +1,11 @@
+
+//Create a Map that stores the names of countries as keys and their capitals as values. Add three countries to the Map.
+
+let CountriesCapital = new Map<string,string>()
+
+CountriesCapital.set("Pakistan","Islamabad")
+CountriesCapital.set("Germany","Berlin")
+CountriesCapital.set("Italy","Rome")
+
+
+console.log(CountriesCapital)

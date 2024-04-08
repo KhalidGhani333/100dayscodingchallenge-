f092a244d82cde25a1a89024ee286b7309a4a86b
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-08 14:25:15 +0500

    Add files via upload

---
+++ day-36/q107/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Write a condition using logical operators that checks if a number is divisible by both 2 and 3.
+function divisibleNumber(number) {
+    return number % 2 === 0 && number % 3 === 0;
+}
+console.log(divisibleNumber(16));
+console.log(divisibleNumber(12));

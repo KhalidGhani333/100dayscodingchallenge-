26ac5ad9975579a0e193695b12c362e4c94c5686
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-16 00:14:12 +0500

    Add files via upload

---
+++ day-42/q126/main.js
@@ -0,0 +1,16 @@
+"use strict";
+//Explain how the this keyword changes its value when used inside a nested function within a method.
+//When this is used inside a nested function within an object's method, it does not refer to the object itself 
+//but to the global object (in a browser, window). This behavior often requires workarounds like assigning this
+// to another variable in the outer function.
+let myObject = {
+    property: "Value",
+    outerMethod: function () {
+        console.log(this.property);
+        let innerMethod = () => {
+            console.log(this.property);
+        };
+        innerMethod();
+    },
+};
+myObject.outerMethod();

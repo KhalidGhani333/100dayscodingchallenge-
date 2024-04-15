096861e5bf6e363573efbc0f3042425a92e55d62
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-16 00:13:02 +0500

    Add files via upload

---
+++ day-42/q124/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Create a function inside an object that returns the object's own name property using the this keyword.
+let personName = {
+    name: "khalid",
+    firstName: function () {
+        return this.name;
+    }
+};
+console.log(personName.firstName());

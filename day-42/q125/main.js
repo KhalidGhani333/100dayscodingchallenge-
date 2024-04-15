9e7d1c581019e1b622d634a6ed3ae74d45a2d615
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-16 00:13:36 +0500

    Add files via upload

---
+++ day-42/q125/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Modify a method in an object to use the this keyword to access another property in the same object.
+const rectangle = {
+    length: 6,
+    width: 8,
+    calculateArea: function () {
+        return this.length * this.width;
+    },
+};
+console.log(rectangle.calculateArea());

f6b9a942f846a90e35eb704943aca01561121b75
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-15 16:56:41 +0500

    Add files via upload

---
+++ day-40/q120/main.ts
@@ -0,0 +1,8 @@
+//Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+
+let favoriteMovie =["Avatar","Venom","KGF","Animal","SpiderMan"]
+
+for(let movies of favoriteMovie){
+    console.log(movies)
+}

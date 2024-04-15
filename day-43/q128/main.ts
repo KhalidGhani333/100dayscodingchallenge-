7edf944b69977c9d57bd44b0d91cea4872a6ace6
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-16 00:18:09 +0500

    Add files via upload

---
+++ day-43/q128/main.ts
@@ -0,0 +1,10 @@
+//Create an arrow function that takes multiple parameters and returns the product of all parameters.
+
+//*Explain & TIP:* Arrow functions can take zero, one, or multiple parameters. When dealing with multiple parameters,
+// you enclose them in parentheses.
+
+
+let ArrowFunction=(...number:number[]) => 
+    number.reduce((total,number)=>total*number )
+
+console.log(ArrowFunction(3,4,5))

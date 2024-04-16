0f2f3ddaa1687523e0a10810bded7c8f80f0899d
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-17 00:36:38 +0500

    Add files via upload

---
+++ day-46/q137/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Implement a try-catch block to handle potential errors in a block of code.
+//*Explain & TIP:* The try-catch statement marks a block of statements to try, and specifies a response,
+// should an exception be thrown. It's a critical feature for robust error handling in JavaScript.
+try {
+    throw new Error("Something went wrong");
+}
+catch (error) {
+    console.log(error.message);
+}

a2215cd28ede2ce19eaa98ff30805887e204da6d
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-17 00:35:58 +0500

    Add files via upload

---
+++ day-46/q136/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Use console.log() to debug and track the value of a variable inside a loop.
+//*Explain & TIP:* console.log() is an invaluable tool for developers to monitor the values of variables,
+// the flow of execution, and to understand the state of their program at various points in time,
+// especially within loops where values change with each iteration.
+for (let i = 0; i <= 10; i++) {
+    console.log("i = " + i);
+}

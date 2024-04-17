1378ca701a232d1d1ea2789c3dc6a23ca9945dad
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-17 16:19:19 +0500

    Add files via upload

---
+++ day-47/q139/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//List three reserved words in JavaScript and create a valid use case for each.
+let myname = "shahzaib";
+console.log(myname);
+function myfuntion(x, y) {
+    console.log(x * y);
+}
+myfuntion(2, 4);
+for (let i = 0; i <= 3; i++) {
+    console.log(i);
+}
---
+++ day-47/q139/main.ts
@@ -0,0 +1,19 @@
+//List three reserved words in JavaScript and create a valid use case for each.
+
+
+let myname="shahzaib"
+console.log(myname)
+
+
+
+
+function myfuntion(x:number,y:number){
+    console.log(x * y )
+}
+myfuntion(2,4)
+
+
+
+for(let i=0; i<=3; i++){
+    console.log(i)
+}

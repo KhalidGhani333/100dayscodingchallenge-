3f1d16e2bddd806b7e0149b284effa15cfee83b9
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-18 16:48:52 +0500

    Add files via upload

---
+++ day-48/q144/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Explain the use of the Promise.all() method with an example.
+//*Explain & TIP:* Promise.all() takes an iterable of Promises and returns a single Promise that resolves 
+//when all of the Promises in the iterable have resolved or when the iterable contains no Promises. 
+//It is rejected if any of the passed Promises are rejected. This method is useful for aggregating the 
+//results of multiple promises.
+let promise1 = Promise.resolve(3);
+let promise2 = 13;
+let promise3 = new Promise((resolve) => {
+    setTimeout(resolve, 100, "do");
+});
+Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+});

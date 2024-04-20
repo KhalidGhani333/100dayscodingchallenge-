46ba1c54abf5ac3a70901e12a332a77b2dfa6a12
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-20 11:28:26 +0500

    Add files via upload

---
+++ day-50/q150/main.js
@@ -0,0 +1,13 @@
+"use strict";
+//Describe how asynchronous callbacks differ from synchronous code execution.
+//*Explain & TIP:* Asynchronous callbacks allow JavaScript to perform long network requests, file operations,
+// or set timers without blocking the main thread, enabling the continuation of code execution. 
+//Synchronous code, in contrast, runs sequentially, blocking further execution until the current operation completes.
+console.log("Before synchronous operation");
+for (let i = 0; i < 1e9; i++) { }
+console.log("After synchronous operation");
+console.log("Before synchronous operation");
+setTimeout(() => {
+    console.log("synchronous operation complete");
+}, 1000);
+console.log("After synchronous operation setup");

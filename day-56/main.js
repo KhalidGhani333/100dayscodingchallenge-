274b1cb292e546f8f67b536ed4a25b59a9a24c85
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-04-30 17:05:01 +0500

    Add files via upload

---
+++ day-56/main.js
@@ -0,0 +1,16 @@
+"use strict";
+//TASK 1
+//The postfix and prefix forms
+let a = 1;
+let b = 1;
+console.log(++a);
+console.log(b++);
+console.log(a);
+console.log(b);
+//Assignment result
+let c = 2;
+let d = 1 + (c *= 2);
+console.log(d);
+let e = "1";
+let f = "2";
+console.log(e + f); // 12
---
+++ day-56/main.ts
@@ -0,0 +1,28 @@
+
+//TASK 1
+//The postfix and prefix forms
+
+let a = 1;
+let b = 1;
+
+console.log(++a)   //2
+console.log(b++)   //1
+
+console.log(a)     //2
+console.log(b)     //2
+
+//TASK 2
+//Assignment result
+
+let c = 2
+let d = 1 + (c *= 2)  // 5
+
+console.log(d)
+
+// TASK 3
+//Fix the addition of number by string
+
+let e = "1"
+let f = "2"
+
+console.log(e + f) ; // 12

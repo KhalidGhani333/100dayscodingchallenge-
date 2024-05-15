8bc6d1bedafefea6a9886bd695ba5d1e4e68ccfd
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-05-15 16:25:49 +0500

    Add files via upload

---
+++ day-73/main.js
@@ -0,0 +1,4 @@
+"use strict";
+//Type Assertions 
+let value = 12345;
+console.log(value);
---
+++ day-73/main.ts
@@ -0,0 +1,9 @@
+
+
+
+
+//Type Assertions 
+
+let value:any = 12345;
+
+console.log((value as string).repeat(3));
